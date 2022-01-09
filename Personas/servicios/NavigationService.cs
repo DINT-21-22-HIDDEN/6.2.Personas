26669e90617c60dc36f11d14195a3b00ddd742d1
Javier Catalá <javicatala@iesdoctorbalmis.com>
2022-01-09 12:31:05 +0100

    Agregar archivos de proyecto.

---
+++ Personas/servicios/NavigationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Personas
+{
+    class NavigationService
+    {
+        private readonly UserControl listadoPersonas;
+
+        public NavigationService()
+        {
+            listadoPersonas = new ListadoPersonasUserControl();
+        }
+
+        public UserControl ObtenerNuevaPersona()
+        {
+            return new NuevaPersonaUserControl();
+        }
+
+        public UserControl ObtenerListadoPersonas()
+        {
+            return listadoPersonas;
+        }
+
+        public bool? AbrirDialogoNacionalidad()
+        {
+            DialogoNacionalidad dialogo = new DialogoNacionalidad();
+            return dialogo.ShowDialog();
+        }
+    }
+}
